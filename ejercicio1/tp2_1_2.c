1b4a4a0867744ee0bb562b37cb6f0d2086d35eed
yahel8666 <yahelcamisay@gmail.com>
2024-04-28 16:26:51 -0300

    ejercicio 1 punto 2

---
+++ ejercicio1/tp2_1_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20
+
+int main()
+{
+    int i;
+    double vt[N];
+    double * pvector = vt; 
+    srand(time(NULL)); 
+    for(i = 0;i<N; i++)
+    {
+        *(pvector+i)= rand()%101;
+        printf("%f \n", *(pvector+i));
+    }
+    return 0;
+}
